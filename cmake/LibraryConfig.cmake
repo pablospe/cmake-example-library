e2a12d3a9fab4f0cced6e05a212375fffa3d4622
Pablo Speciale <pablo.speciale@gmail.com>
2018-07-05 11:32:22 +0200

    Set a default build type ("Release"), if none was specified. Show possible options on 'cmake-gui' for 'CMAKE_BUILD_TYPE' variable: "Debug", "Release", "MinSizeRel" or "RelWithDebInfo".

--- cmake/LibraryConfig.cmake
+++ cmake/LibraryConfig.cmake
@@ -7,6 +7,16 @@ else()
   set(LIBRARY_TYPE STATIC)
 endif()
 
+# Set a default build type if none was specified
+if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+  message(STATUS "Setting build type to 'Release'.")
+  set(CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build." FORCE)
+
+  # Set the possible values of build type for cmake-gui
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
+    "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+endif()
+
 # Target
 add_library(${LIBRARY_NAME} ${LIBRARY_TYPE} ${SOURCES} ${HEADERS})
 
